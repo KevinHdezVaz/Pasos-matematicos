83bec4719d90925d929678f34f714af6a343704f
KevinHdezVaz <kevinhdezvaz@gmail.com>
2021-05-11 16:23:11 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+ 
+import Demo from './components/Demo.jsx'
+import './styles/App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+          <div className='App'>
+          <div className='App-header'>
+             <h2>EasyMath</h2>
+           
+          </div>
+          <Demo/>
+         </div>
+      </MuiThemeProvider>
+    );
+  }
+}
---
+++ src/components/Steps.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import TeX from '../react-components/tex.jsx';
+import mathsteps from 'mathsteps';
+
+import print from '../print.js';
+import Step from './Step.jsx';
+import '../styles/steps.css';
+
+export default class Steps extends Component {
+  static propTypes = {
+    input: PropTypes.string
+  };
+
+  isEquation(mathInput) {
+    const comparators = ['<=', '>=', '=', '<', '>'];
+    let isEquation = false;
+
+    comparators.forEach(comparator => {
+      if (mathInput.includes(comparator)) isEquation = true;
+    });
+    return isEquation;
+  }
+
+  renderSteps = (steps) => {
+    const renderedSteps = steps.map(
+      (step, index) => <Step step={step} key={index}/>);
+    return <div>
+      <div className='latex'><TeX>{print.oldNode(steps[0])}</TeX></div>
+      {renderedSteps}
+    </div>;
+  }
+
+  render() {
+    const {input} = this.props;
+    const isEquation = this.isEquation(input);
+    const steps = isEquation
+      ? mathsteps.solveEquation(input)
+      : mathsteps.simplifyExpression(input);
+
+    if (steps.length === 0) {
+      return <div className='error'>
+       No hay pasos para esta entrada :( <br/> <br/>
+        Esto probablemente se deba a que: <br/>
+        1. Tu novia te dejo <br/>
+        2. Esto ya está simplificado / resuelto <br/>
+        3. Tuvimos problemas para analizar su entrada.
+      </div>;
+    }
+
+    return <div className='steps'>
+      {this.renderSteps(steps)}
+    </div>;
+  }
+}
